3b19eddc1a186e6cbb1dd533c64332d731f15442
John Doe <johndoe@example.com>
2024-04-24 03:49:56 +0000

    Including docstring in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     maintainer_email="dsadasiv@andrew.cmu.edu",
     license="MIT",
     packages=["coys"],
-    long_description="""A long
-      multiline description.""",
+    long_description="""A module containing functions 
+      for calculating properties of peptide sequences, summarizing sequence properties, 
+      and generating sequences using genetic algorithms. 
+      The `calculate_properties()` function computes various physicochemical 
+      properties of peptide sequences. The `sequence_summary` provides a summary of these 
+      properties and visualizes the amino acid distribution. 
+      The `generate_seq` function utilizes a genetic algorithm to generate peptide sequences 
+      optimized for user-defined properties. This module relies on external libraries such 
+      ModlAMP for obtaining the individual protein properties, and pyGAD for implementing the genetic algorithm.""",
 )
